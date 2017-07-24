7c020d82e7b8fc42cd46f8b76d08e4ecbea261c8
Joann Won <joannwon0123210@gmail.com>
2017-07-24 15:50:26 -0700

    SecondActivity AddButtons
    Added Buttons and restrained the texts and buttons

--- app/src/main/java/com/example/errorproject/SecondActivity.java
+++ app/src/main/java/com/example/errorproject/SecondActivity.java
@@ -2,12 +2,35 @@ package com.example.errorproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
+    TextView quote;
+    Button clk;
+    EditText et;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+       /* quote = (TextView) findViewById(R.id.textview);
+        clk = (Button) findViewById(R.id.button);
+        et = (EditText) findViewById(R.id.editText); */
     }
+
+    /*Button button = (Button) findViewById(R.id.Edit);
+    button.setOnClickListener(new View.OnClickListener() {
+        public void onClick(View v) {
+            public void changeTrackerName1(View v){
+                String st = et.getText().toString();
+                quote.setText(st);
+            }
+            // Do something in response to button click
+        }
+    }; */
+
 }
